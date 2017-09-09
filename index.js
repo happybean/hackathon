791bbfc98958d58fcc675780e7174b8df9c154f0
happybean <happybean.shih@gmail.com>
2017-09-09 12:39:10 +0800

    first

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ var bot = linebot({
 	channelAccessToken:'HrgD9tlcM3S9nLmvLOqfJx4J1k7wOGUS+USrl1DAFxmHtyGn6oPsFpCG5QStnoNgyAfU7bwx/f8Yw5jn7Y4k0LlY3lY18keCA9HD/CmRbI6PhXJJBqCob5cDq6N7psKKMf80mP0atPFalbT0/HKnjwdB04t89/1O/w1cDnyilFU='
 });
 
+
 bot.on('message', function(event) {
-  console.log(event); //把收到訊息的 event 印出來看看
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+  }
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log('目前的port是', port);
 });
 
 const app = express();
